9e2b02df75c2a2d4a50b12c1c08435d72a1fd592
Daniel <kosmindaniel@gmail.com>
2025-05-16 10:02:21 -0400

    e

--- src/test/java/com/kosmin/authorization/integration/test/BaseIntegrationTest.java
+++ src/test/java/com/kosmin/authorization/integration/test/BaseIntegrationTest.java
@@ -3,7 +3,6 @@ package com.kosmin.authorization.integration.test;
 import com.kosmin.authorization.repository.UserRepository;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
